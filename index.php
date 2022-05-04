b0e467324c37331f3299402a741641e40f7b5f8d
Le Sy Dan <les26dan@gmail.com>
2022-05-04 17:02:23 +0700

    Add new project food

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <title>Web phụ kiện điện thoại</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <img src="images/banner.jpg">
+        </div>
+        <div class="menu">
+            <p>menu</p>
+        </div>
+        <div class="main">
+            <div class="sidebar"> sidebar</div>
+            <div class="maincontent"> maincontent</div>
+        </div>
+        <div class="clear"></div>
+        <div class="footer">
+            <p>footer</p>
+        </div>
+    </div>
+</body>
+</html>
